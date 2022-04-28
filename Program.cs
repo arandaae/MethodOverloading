460a8ab6b8f35f2b0e1ab91a48034c8eb698c43e
Armando Aranda <arandaae19@gmail.com>
2022-04-28 10:36:05 -0500

    Frist Commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Add(2, 4);
+           Add(3.5, 5.3);
+           Console.WriteLine(Add(2, 2, true));
+        }
+
+        public static int Add(int x, int y)
+        {
+           Console.WriteLine(x + y);
+           return x + y;          
+        }
+
+        public static double Add(double x, double y)
+        {
+            Console.WriteLine(x + y);
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool verify)
+        {
+            int sum = x + y;
+
+            if (verify == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if(verify == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (verify == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
+    }
+}
